e1e341868cbb6b37cacd97b269f8fa830edd64a7
James Slocum <jslocum@telvue.com>
2012-03-29 09:58:10 -0400

    Added jsontools.c to project

---
+++ jsontools.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#include "JSONTools.h"
+
+int main(int argc, char* argv[]){
+   printf("I just took in %d arguments\n", argc);
+   return EXIT_SUCCESS;
+}
+
